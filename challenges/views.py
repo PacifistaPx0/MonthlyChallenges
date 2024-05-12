bdce37cc05408cd64415fc8fa302dea189c295a8
PacifistaPX0 <richieadigwe@gmail.com>
2024-05-12 20:28:57 +0100

    template langauge and variable interpolation

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-# Create your views here.
+
+
+
 month_dict = {
     "january": "This is January",
     "february": "This is February",
@@ -17,6 +19,9 @@ month_dict = {
     "december": "This is December"
 }
 
+# Create your views here.
+
+
 def index(request):
     list_items = ""
     months = list(month_dict.keys())
@@ -24,7 +29,8 @@ def index(request):
     for month in months:
         capitalized_month = month.capitalize()
         month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+        list_items += f"<li><a href=\"{month_path}\">{
+            capitalized_month}</a></li>"
 
     # "<li><a href=""...">january</a><li><a href="...">february</a>...
 
@@ -32,24 +38,27 @@ def index(request):
 
     return HttpResponse(response_data)
 
+
 def monthly_challenge_by_number(request, month):
 
     try:
-        if month==0:
+        if month == 0:
             return HttpResponseNotFound("Month does not exist")
         redirect_month = list(month_dict.keys())[month-1]
-        redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/january
+        redirect_path = reverse(
+            "month-challenge", args=[redirect_month])  # /challenge/january
         return HttpResponseRedirect(redirect_path)
     except:
         return HttpResponseNotFound("Month does not exist")
 
 
 def monthly_challenge(request, month):
-    try:
+    #try:
         challenge_text = month_dict[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
-    except:
-        return HttpResponseNotFound("<h1>This month does not exist</h1>")
-    
-
+        redirect_month = month.capitalize()
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month": redirect_month
+        })
+    #except:
+        #return HttpResponseNotFound("<h1>This month does not exist</h1>")
--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'challenges', #installing our app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
